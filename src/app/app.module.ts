a29a9a66f5175cd7d1cd0bb7a37e84bebf7b9055
asmasyakirah <asma.syakirahd@gmail.com>
2020-01-04 18:47:35 +0800

    final commit. added bootstrap4. stop at adding linking and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule // Add Bootstrap module here.
   ],
   providers: [],
   bootstrap: [AppComponent]
